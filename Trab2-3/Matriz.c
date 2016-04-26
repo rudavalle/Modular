0172718109cd2515bf552fc36cbe7a44e258f819
Hugo Machado <hugomachado2005@hotmail.com>
2016-04-26 20:02:17 -0300

    Update Matriz.c

--- Trab2-3/Matriz.c
+++ Trab2-3/Matriz.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <malloc.h>
 
 #include "PILHA.H"
 #include "Matriz.h"
+#include "LISTA.H"
 
-typedef struct tgNo {
+typedef struct tgNo{
 
 	int linha;
 
@@ -13,7 +14,7 @@ typedef struct tgNo {
 	struct tgNo * norte;
 
 	struct tgNo * sul;
-
+	
 	struct tgNo * leste;
 
 	struct tgNo * oeste;
@@ -22,8 +23,8 @@ typedef struct tgNo {
 
 }tpNo;
 
-typedef struct tgMatriz {
-
+typedef struct tgMatriz{
+	
 	tpNo * noRaiz;
 
 	tpNo * noCorr;
@@ -36,22 +37,22 @@ typedef struct tgMatriz {
 
 /***** Protótipos das funções encapuladas no módulo *****/
 
-static void destroiNos(tpNo * no);
+ static void destroiNos( tpMatriz * mat );
 
-static void moverCorrParaRaiz(tppMatriz mat);
+ static void moverCorrParaRaiz(tppMatriz mat);
 
-/*****  Código das funções exportadas pelo módulo  *****/
+ /*****  Código das funções exportadas pelo módulo  *****/
 
 
-MAT_tpCondRet criaMatriz(tppMatriz * mat) {
-
-	if (mat == NULL) {
+MAT_tpCondRet criaMatriz(tppMatriz * mat){
+	
+	if(mat == NULL){
 		return MAT_CondRetMatrizNaoExiste;
 	}
 
 	*mat = (tpMatriz*)malloc(sizeof(tpMatriz));
 
-	if (mat == NULL) {
+	if(mat == NULL){
 		return MAT_CondRetFaltouMemoria;
 	}
 
@@ -63,108 +64,106 @@ MAT_tpCondRet criaMatriz(tppMatriz * mat) {
 	return MAT_CondRetOK;
 }
 
-void moverCorrParaRaiz(tppMatriz mat) {
+void moverCorrParaRaiz(tppMatriz mat){
 
 	mat->noCorr = mat->noRaiz;
 
 }
 
-MAT_tpCondRet inserirLinha(tppMatriz mat) {
+MAT_tpCondRet inserirLinha(tppMatriz mat){
 	int i;
 	tpNo * temp;
 
 	mat->qLinha++;
-
-	if (mat == NULL) {
-		return MAT_CondRetMatrizNaoExiste;
+	
+	if(mat == NULL){
+		return MAT_CondRetMatrizVazia;
 	}
 
-	if (mat->noRaiz == NULL) {
+	if(mat->noRaiz == NULL){
 		tpNo * novo = (tpNo*)malloc(sizeof(tpNo));
-
-		if (novo == NULL) {
+		
+		if(novo == NULL){
 			return MAT_CondRetFaltouMemoria;
 		}
 		mat->qColuna++;
 
 		novo->coluna = mat->qColuna;
 		novo->linha = mat->qLinha;
-
+		
 		novo->norte = NULL;
 		novo->sul = NULL;
 		novo->leste = NULL;
 		novo->oeste = NULL;
-
+		
 		novo->pilha = NULL;
 
 		mat->noRaiz = novo;
 		mat->noCorr = novo;
-
-	}
-	else {
-
+		
+	}else {
+		
 		temp = mat->noRaiz;
-
-		while (temp->sul != NULL) {
-			temp = temp->sul;
-		}
-
-		for (i = 0; i < (mat->qColuna); i++) {
-
-			tpNo * novo = (tpNo*)malloc(sizeof(tpNo));
-
-			if (novo == NULL) {
-				return MAT_CondRetFaltouMemoria;
+			
+			while(temp->sul != NULL){
+				temp = temp->sul;
 			}
+			
+			for(i=0;i < (mat->qColuna); i++){
 
-			novo->linha = mat->qLinha;
-			novo->coluna = (i + 1);
+				tpNo * novo = (tpNo*)malloc(sizeof(tpNo));
+				
+				if(novo == NULL){
+					return MAT_CondRetFaltouMemoria;
+				}
 
-			novo->pilha = NULL;
+				novo->linha = mat->qLinha;
+				novo->coluna = (i + 1);
 
-			mat->noCorr = novo;
+				novo->pilha = NULL;
 
-			novo->norte = temp;
-			temp->sul = novo;
+				mat->noCorr = novo;
 
-			if (temp->oeste == NULL) {
-				novo->oeste = NULL;
+				novo->norte = temp;
+				temp->sul = novo;
 
+				if(temp->oeste == NULL){
+					novo->oeste = NULL;
+
+				}else {
+					novo->oeste = temp->oeste->sul;
+					novo->norte->oeste->sul->leste = novo;
+				}
+
+				novo->leste = NULL;
+
+				novo->sul = NULL;
+
+				if(temp->leste != NULL){
+					temp = temp->leste;
+				}
 			}
-			else {
-				novo->oeste = temp->oeste->sul;
-				novo->norte->oeste->sul->leste = novo;
-			}
-
-			novo->leste = NULL;
-
-			novo->sul = NULL;
-
-			if (temp->leste != NULL) {
-				temp = temp->leste;
-			}
-		}
 	}
 
 	return MAT_CondRetOK;
 
 }
 
-MAT_tpCondRet inserirColuna(tppMatriz mat) {
-
+MAT_tpCondRet inserirColuna(tppMatriz mat){
+	
 	int i;
 	tpNo * temp;
-
-	if (mat == NULL) {
-		return MAT_CondRetMatrizNaoExiste;
+	
+	if(mat == NULL){
+		return MAT_CondRetMatrizVazia;
 	}
 
 	mat->qColuna++;
-
-	if (mat->noRaiz == NULL) {
+	
+	if(mat->noRaiz == NULL){
 		tpNo * novo = (tpNo*)malloc(sizeof(tpNo));
-
-		if (novo == NULL) {
+		
+		if(novo == NULL){
 			return MAT_CondRetFaltouMemoria;
 		}
 
@@ -176,54 +175,52 @@ MAT_tpCondRet inserirColuna(tppMatriz mat) {
 		novo->sul = NULL;
 		novo->leste = NULL;
 		novo->oeste = NULL;
-
+		
 		novo->pilha = NULL;
-
+		
 		mat->noRaiz = novo;
 		mat->noCorr = novo;
-
-	}
-	else {
-
+	
+	}else {
+		
 		temp = mat->noRaiz;
-
-		while (temp->leste != NULL) {
-			temp = temp->leste;
-		}
-
-		for (i = 0; i < mat->qLinha; i++) {
-			tpNo * novo = (tpNo*)malloc(sizeof(tpNo));
-
-			if (novo == NULL) {
-				return MAT_CondRetFaltouMemoria;
+			
+			while(temp->leste != NULL){
+				temp = temp->leste;
 			}
+			
+			for(i=0;i < mat->qLinha; i++){
+				tpNo * novo = (tpNo*)malloc(sizeof(tpNo));
+				
+				if(novo == NULL){
+					return MAT_CondRetFaltouMemoria;
+				}
 
-			novo->coluna = mat->qColuna;
-			novo->linha = mat->qLinha;
+				novo->coluna = mat->qColuna;
+				novo->linha = mat->qLinha;
 
-			novo->pilha = NULL;
-			mat->noCorr = novo;
+				novo->pilha = NULL;
+				mat->noCorr = novo;
 
-			novo->oeste = temp;
-			temp->leste = novo;
+				novo->oeste = temp;
+				temp->leste = novo;
 
-			if (temp->norte == NULL) {
-				novo->norte = NULL;
+				if(temp->norte == NULL){
+					novo->norte = NULL;
+				}else {
+					novo->norte = temp->norte->leste;
+					novo->norte->sul = novo;
+				}
+
+				novo->leste = NULL;
+
+				novo->sul = NULL;
+
+				if(temp->sul != NULL){
+					temp = temp->sul;
+				}
+
 			}
-			else {
-				novo->norte = temp->norte->leste;
-				novo->norte->sul = novo;
-			}
-
-			novo->leste = NULL;
-
-			novo->sul = NULL;
-
-			if (temp->sul != NULL) {
-				temp = temp->sul;
-			}
-
-		}
 	}
 
 
@@ -231,31 +228,31 @@ MAT_tpCondRet inserirColuna(tppMatriz mat) {
 
 }
 
-MAT_tpCondRet criarTabuleiro(tppMatriz * mat, int numColuna, int numLinha) {
-
+MAT_tpCondRet criarTabuleiro(tppMatriz * mat, int numColuna, int numLinha){
+	
 	int i;
-
+	
 	MAT_tpCondRet CondRet;
 
 	CondRet = criaMatriz(&(*mat));
 
-	if (CondRet != MAT_CondRetOK) {
+	if(CondRet != MAT_CondRetOK){
 		return CondRet;
 	}
 
-	for (i = 0; i < numColuna; i++) {
+	for(i=0;i < numColuna; i++){
 		CondRet = inserirColuna(*mat);
 	}
 
-	if (CondRet != MAT_CondRetOK) {
+	if(CondRet != MAT_CondRetOK){
 		return CondRet;
 	}
 
-	for (i = 1; i < numLinha; i++) {
+	for(i=1;i < numLinha; i++){
 		CondRet = inserirLinha(*mat);
 	}
 
-	if (CondRet != MAT_CondRetOK) {
+	if(CondRet != MAT_CondRetOK){
 		return CondRet;
 	}
 
@@ -265,17 +262,17 @@ MAT_tpCondRet criarTabuleiro(tppMatriz * mat, int numColuna, int numLinha) {
 
 }
 
-MAT_tpCondRet moverSul(tppMatriz mat) {
+MAT_tpCondRet moverSul(tppMatriz mat){
 
-	if (mat == NULL) {
+	if(mat == NULL){
 		return MAT_CondRetMatrizNaoExiste;
 	}
-	/*
-	if (mat->noRaiz == NULL) {
-		return MAT_CondRetMatrizVazia;
-	}*/
 
-	if (mat->noCorr->sul == NULL) {
+	if(mat->noRaiz == NULL){
+		return MAT_CondRetMatrizVazia;
+	}
+
+	if(mat->noCorr->sul == NULL){
 		return MAT_CondRetNoNaoExiste;
 	}
 
@@ -285,17 +282,17 @@ MAT_tpCondRet moverSul(tppMatriz mat) {
 
 }
 
-MAT_tpCondRet moverLeste(tppMatriz mat) {
-
-	if (mat == NULL) {
+MAT_tpCondRet moverLeste(tppMatriz mat){
+	
+	if(mat == NULL){
 		return MAT_CondRetMatrizNaoExiste;
 	}
-	/*
-	if (mat->noRaiz == NULL) {
+
+	if(mat->noRaiz == NULL){
 		return MAT_CondRetMatrizVazia;
-	}*/
-	
-	if (mat->noCorr->leste == NULL) {
+	}
+
+	if(mat->noCorr->leste == NULL){
 		return MAT_CondRetNoNaoExiste;
 	}
 
@@ -304,161 +301,156 @@ MAT_tpCondRet moverLeste(tppMatriz mat) {
 	return MAT_CondRetOK;
 
 }
-/* * * * * * * * * * * * * * * * * * * * * * *
-	FUNÇÕES QUE SERAM USADAS PARA O TRABALHO 3
 
-*MAT_tpCondRet moverOeste(tppMatriz mat) {
-*
-*	if (mat == NULL) {
-*		return MAT_CondRetMatrizNaoExiste;
-*	}
-*
-*	if (mat->noRaiz == NULL) {
-*		return MAT_CondRetMatrizVazia;
-*	}
-*
-*	if (mat->noCorr->leste == NULL) {
-*		return MAT_CondRetNoNaoExiste;
-*	}
-*
-*	mat->noCorr = mat->noCorr->oeste;
-*
-*	return MAT_CondRetOK;
-*
-*}
-*
-* * * * * * * * * * * * * * * * * * * * * *  */
+MAT_tpCondRet moverOeste(tppMatriz mat){
+	
+	if(mat == NULL){
+		return MAT_CondRetMatrizNaoExiste;
+	}
 
-/* * * * * * * * * * * * * * * * * * * * * *
-*	FUNÇÕES QUE SERAM USADAS NO TRABALHO 3
-*
-*MAT_tpCondRet moverNorte(tppMatriz mat) {
-*	if (mat == NULL) {
-*		return MAT_CondRetMatrizNaoExiste;
-*	}
-*
-*	if (mat->noRaiz == NULL) {
-*		return MAT_CondRetMatrizVazia;
-*	}
-*
-*	if (mat->noCorr->leste == NULL) {
-*		return MAT_CondRetNoNaoExiste;
-*	}
-*
-*	mat->noCorr = mat->noCorr->norte;
-*
-*	return MAT_CondRetOK;
-*
-*}
-* * * * * * * * * * * * * * * * * * * * * * * */
-MAT_tpCondRet empilhaValor(tppMatriz mat, void * valor, int lin, int col) {
+	if(mat->noRaiz == NULL){
+		return MAT_CondRetMatrizVazia;
+	}
+
+	if(mat->noCorr->oeste == NULL){
+		return MAT_CondRetNoNaoExiste;
+	}
+
+	mat->noCorr = mat->noCorr->oeste;
+
+	return MAT_CondRetOK;
+
+}
+
+MAT_tpCondRet moverNorte(tppMatriz mat){
+	if(mat == NULL){
+		return MAT_CondRetMatrizNaoExiste;
+	}
+
+	if(mat->noRaiz == NULL){
+		return MAT_CondRetMatrizVazia;
+	}
+
+	if(mat->noCorr->norte == NULL){
+		return MAT_CondRetNoNaoExiste;
+	}
+	
+	mat->noCorr = mat->noCorr->norte;
+
+	return MAT_CondRetOK;
+
+}
+
+MAT_tpCondRet empilharValor(tppMatriz mat, void * valor, int lin, int col){
 
 	MAT_tpCondRet CondRetm;
 
 	PIL_tpCondRet CondRetp;
-
-	while (mat->noCorr->linha != lin) {
+	
+	while(mat->noCorr->linha != lin){
 
 		CondRetm = moverSul(mat);
 	}
-
-	if (CondRetm != MAT_CondRetOK) {
-		return CondRetm;
+	if(lin != 1){
+		if(CondRetm != MAT_CondRetOK){
+			return CondRetm;
+		}
 	}
 
-	while (mat->noCorr->coluna != col) {
+	while(mat->noCorr->coluna != col){
 		CondRetm = moverLeste(mat);
 	}
 
-	if (CondRetm != MAT_CondRetOK) {
-		return CondRetm;
+	if(col != 1){
+		if(CondRetm != MAT_CondRetOK){
+			return CondRetm;
+		}
 	}
 
-	if (mat->noCorr->pilha == NULL) {
+	if(mat->noCorr->pilha == NULL){
 		tppPilha p;
-
+		
 		CondRetp = criaPilha(&p);
 
 		pilhaPush(p, (void *)valor);
 
 		mat->noCorr->pilha = p;
 
-		if (CondRetp != MAT_CondRetOK) {
+		if(CondRetp != MAT_CondRetOK){
 			return MAT_CondRetFaltouMemoria;
 		}
-
 	}
-	else {
-
-		CondRetp = pilhaPush(mat->noCorr->pilha, valor);
-
+	else 
+	{
+		CondRetp = pilhaPush(mat->noCorr->pilha, (void*)valor);
 	}
 
-	if (CondRetp == PIL_CondRetPilhaVazia) {
-
+	if(CondRetp == PIL_CondRetPilhaVazia){
+		
 		return MAT_CondRetPilhaVazia;
 	}
 
-	if (CondRetp == PIL_CondRetFimPilha) {
-
+	if(CondRetp == PIL_CondRetFimPilha){
+		
 		return MAT_CondRetFimPilha;
 	}
-
+	
 	moverCorrParaRaiz(mat);
 
-	return CondRetm;
-
+	return MAT_CondRetOK;
+	
 }
 
-MAT_tpCondRet desempilhaValor(tppMatriz mat, void ** valor, int lin, int col) {
+MAT_tpCondRet desempilharValor(tppMatriz mat, int lin, int col){
 
 	MAT_tpCondRet CondRetm;
 
 	PIL_tpCondRet CondRetp;
 
-	while (mat->noCorr->linha != lin) {
+	while(mat->noCorr->linha != lin){
 
 		CondRetm = moverSul(mat);
 	}
 
-	if (CondRetm != MAT_CondRetOK) {
-		return CondRetm;
+	if(lin != 1){
+		if(CondRetm != MAT_CondRetOK){
+			return CondRetm;
+		}
 	}
-
-	while (mat->noCorr->coluna != col) {
+	
+	while(mat->noCorr->coluna != col){
 		CondRetm = moverLeste(mat);
 	}
 
-	if (CondRetm != MAT_CondRetOK) {
-		return CondRetm;
+	if(lin != 1){
+		if(CondRetm != MAT_CondRetOK){
+			return CondRetm;
+		}
 	}
 
-	CondRetp = pilhaPop(mat->noCorr->pilha, valor);
+	if(mat->noCorr->pilha == NULL){
+		return MAT_CondRetPilhaVazia;
+	}
+	
+	CondRetp = pilhaPop(mat->noCorr->pilha);
 
-	if (CondRetp == PIL_CondRetPilhaVazia) {
-
+	if(CondRetp != MAT_CondRetOK){
 		return MAT_CondRetPilhaVazia;
 	}
 
-	if (CondRetp == PIL_CondRetFimPilha) {
-
-		return MAT_CondRetFimPilha;
-	}
-
-
 	moverCorrParaRaiz(mat);
+
+	return MAT_CondRetOK;
 
 }
 
 
-MAT_tpCondRet destroiMatriz(tppMatriz mat) {
-
-	if (mat != NULL) {
-		destroiNos(mat->noCorr);
-
+MAT_tpCondRet destruirMatriz(tppMatriz mat){
+	
+	if(mat != NULL){
+		destroiNos(mat);
 		free(mat);
-	}
-	else {
+	}else {
 		return MAT_CondRetMatrizNaoExiste;
 	}
 
@@ -466,57 +458,25 @@ MAT_tpCondRet destroiMatriz(tppMatriz mat) {
 
 }
 
-void destroiNos(tpNo * no) {
 
-	if (no->leste != NULL)
-	{
-		destroiNos(no->leste);
+void destroiNos(tpMatriz * mat){
+	int i;
+	int j=0;
+	int z;
+
+	moverCorrParaRaiz(mat);
+	
+	for(i=0;i<(mat->qColuna);i++){
+			free(mat->noCorr);
+			moverLeste(mat);
+
+		z=i;
+		z++;
+		moverCorrParaRaiz(mat);
+		while(j<z){
+			moverSul(mat);
+			j++;
+		}
+		j=0;
 	}
-
-
-	if (no->norte != NULL)
-	{
-		destroiNos(no->norte);
-	}
-
-	if (no->oeste != NULL)
-	{
-		destroiNos(no->oeste);
-	}
-
-	if (no->sul != NULL)
-	{
-		destroiNos(no->sul);
-	}
-
-	free(no);
-
-}
-
-int main(void) {
-	int i = 1;
-	int j = 2;
-	int z = 3;
-	int teste;
-	tppPilha p;
-	MAT_tpCondRet CondRet;
-
-	tpMatriz * mat;
-
-	criarTabuleiro(&mat, 4, 4);
-
-	empilhaValor(mat, (void*)i, 4, 3);
-
-	empilhaValor(mat, (void*)j, 4, 3);
-
-	empilhaValor(mat, (void*)z, 4, 4);
-
-	desempilhaValor(mat, (void**)&teste, 4, 4);
-
-
-	printf("%d\n", teste);
-
-
-	return 0;
-
 }
